f63061d62977239f17ed77e955a5d98f8c57b728
Sławomir Czerwiński <slawomir.czerwinski@javeo.eu>
2016-06-14 14:23:29 +0200

    Implemented equals and hashCode in Either

--- app/src/main/kotlin/eu/javeo/talk/kotlin/disjoint/Either.kt
+++ app/src/main/kotlin/eu/javeo/talk/kotlin/disjoint/Either.kt
@@ -4,9 +4,13 @@ abstract class Either<L, R> private constructor() {
 
 	class Left<L>(val value: L) : Either<L, Nothing>() {
 		override fun toString() = "Left($value)"
+		override fun equals(other: Any?): Boolean = other is Left<*> && other.value == value
+		override fun hashCode(): Int = value?.hashCode() ?: 0
 	}
 
 	class Right<R>(val value: R) : Either<Nothing, R>() {
 		override fun toString() = "Right($value)"
+		override fun equals(other: Any?): Boolean = other is Right<*> && other.value == value
+		override fun hashCode(): Int = value?.hashCode() ?: 0
 	}
 }
